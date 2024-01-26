3f92ec98bab06db126b50a8efd79b61c4d23ad2f
Sricharan2211 <tudugani.charan@mphasis.com>
2024-01-26 18:18:35 +0530

    UI Customers

---
+++ UI/Models/QuestionView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UI.Models
+{
+    public class QuestionView
+    {
+
+        public int QuestionId { get; set; }
+        [MaxLength(255)] 
+        public string Question { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; } = DateTime.Now; 
+
+        [MaxLength(255)] 
+        public string Answer { get; set; }
+
+        [Required(ErrorMessage = "Customer Id is required.")]
+        public int CustomerId { get; set; }
+    }
+}
